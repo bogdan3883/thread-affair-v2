dbf6cb0a99b8acea226d42fd930e0ce5409fb492
Bogdan <93480175+bogdan3883@users.noreply.github.com>
2022-07-27 10:02:25 +0300

    2707

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
+import ContentBanner from '../components/homepage/ContentBanner';
 import HomeNewsletter from '../components/homepage/HomeNewsletter';
 import HomePayment from '../components/homepage/HomePayment';
 import HomeSocial from '../components/homepage/HomeSocial';
@@ -15,9 +16,15 @@ export default function Home() {
       <Header></Header>
 
       <main className="content">
-        <HomeNewsletter></HomeNewsletter>
-        <HomeSocial></HomeSocial>
-        <HomePayment></HomePayment>
+        <header className="content-header mb-5">
+          <ContentBanner></ContentBanner>
+        </header>
+
+        <section className="content-main">
+          <HomeNewsletter></HomeNewsletter>
+          <HomeSocial></HomeSocial>
+          <HomePayment></HomePayment>
+        </section>
       </main>
 
       <Footer></Footer>
